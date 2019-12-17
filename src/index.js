406fb17543bb78b5af3a60d7cd9534baef8ea708
Majid Kareem <43646844+Emkay860@users.noreply.github.com>
2019-12-17 15:02:38 +0100

    Added context.js to src folder
    Added context.js and created the ProductProvider and ProductConsumer variables to help in hold data using the flux pattern similar to react-redux.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.js";
+import { ProductProvider } from "./context";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <ProductProvider>
+    <Router>
+      <App />
+    </Router>
+  </ProductProvider>,
   rootElement
 );
