d4dfe1c9de3771ed5a95af7eddd1f96ae348b00c
Majid Kareem <43646844+Emkay860@users.noreply.github.com>
2019-12-17 15:29:20 +0100

    Updated Product.js
    added a ProductWrapper sty;ed-component to it. and setup an onClick() method to link to the product details page

--- src/context.js
+++ src/context.js
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
+import { storeProducts, detailProduct } from "./data";
 
 const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
+  state = {
+    products: storeProducts,
+    detailProduct: detailProduct
+  };
+
+  handleDetail = () => {
+    console.log("Hello from detail");
+  };
+
+  addToCart = () => {
+    console.log("Hello from add to  Cart");
+  };
+
   render() {
     return (
-      <ProductContext.Provider value="Hello from Context">
+      <ProductContext.Provider
+        value={{
+          ...this.state,
+          handleDetail: this.handleDetail,
+          addToCart: this.addToCart
+        }}
+      >
         {this.props.children}
       </ProductContext.Provider>
     );
@@ -14,4 +34,4 @@ class ProductProvider extends Component {
 
 const ProductConsumer = ProductContext.Consumer;
 
-export {ProductProvider, ProductConsumer};
+export { ProductProvider, ProductConsumer };
