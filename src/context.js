e5406b6c34eb6e9a66878c4da4fdb08322615c1b
Majid Kareem <43646844+Emkay860@users.noreply.github.com>
2019-12-23 07:29:33 +0100

    Updated context.js
    Addded a "cart" array to this.state, completed addToCart() function

--- src/context.js
+++ src/context.js
@@ -6,7 +6,8 @@ const ProductContext = React.createContext();
 class ProductProvider extends Component {
   state = {
     products: [],
-    detailProduct: detailProduct
+    detailProduct: detailProduct,
+    cart: []
   };
 
   componentDidMount() {
@@ -24,12 +25,32 @@ class ProductProvider extends Component {
     });
   };
 
-  handleDetail = () => {
-    console.log("Hello from detail");
+  getItem = id => {
+    const product = this.state.products.find(item => item.id === id);
+    return product;
+  };
+
+  handleDetail = id => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return { detailProduct: product };
+    });
   };
 
   addToCart = id => {
-    console.log(`Hello from add to  Cart.id is ${id}`);
+    let tempProducts = [...this.state.products];
+    const index = tempProducts.indexOf(this.getItem(id));
+    const product = tempProducts[index];
+    product.inCart = true;
+    product.count = 1;
+    const price = product.price;
+    product.total = price;
+
+    this.setState(() => {
+      return { products: tempProducts, cart: [...this.state.cart, product] };
+    },()=>{
+console.log(this.state);
+    });
   };
 
   render() {
